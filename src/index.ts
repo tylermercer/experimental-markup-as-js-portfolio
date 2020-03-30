88f9025ed44f8c3f395b2f11cbd3d813f1dfe79c
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-30 12:12:31 -0600

    Got basic landing page set up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-import { h1, header, p } from 'markup-as-js';
+import { h1, h2, header, p, nav, a, ul, li, b } from 'markup-as-js';
 
 const app = header(
+  nav(
+    h2(a({href: "/"}, "Tyler Mercer")),
+    ul(
+      li(a({href: "/"}, "Home")),
+      li(a({href: "/projects"}, "Projects")),
+      li(a({href: "/blog"}, "Blog")),
+    )
+  ),
   h1("Tyler Mercer"),
   p("Humanistic Software Engineer"),
+  p(a({href: "/projects"}, b("View my projects")))
 );
 
 document.getElementById("app")?.appendChild(app);
