d6fa0118e246da9b38c594df8c170f5cc2f9016a
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-30 13:46:27 -0600

    Make projects list convert names to title case

--- src/routes/ProjectsPage.ts
+++ src/routes/ProjectsPage.ts
@@ -2,12 +2,19 @@ import { h1, header, h3, section, a, aside, p, small } from 'markup-as-js';
 
 const QUERY = 'https://api.github.com/users/tylermercer/repos?sort=pushed&direction=desc';
 
+const prettify = (name: string) => {
+  return name.replace(/-/g, " ").replace(/\w\S*/g, (txt:string) =>{
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+
 const getProjects = () => fetch(QUERY)
   .then(res => res.json())
   .then(repos => section(
     ...repos.filter((r:any) => r.description).map((r:any) => aside(
-      h3(a({href: r.html_url, target: "_blank"}, r.name)),
+      h3(a({href: r.homepage || r.html_url, target: "_blank"}, prettify(r.name))),
       p((r.description || "")),
+      p(a({href:r.html_url}, "View on GitHub")),
       p(small(r.language))
     ))
   ));
