1e87054f0c3e6a028d3ca57f34fafd025b7d9f4d
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-30 14:26:06 -0600

    Update tagline

--- src/routes/MainPage.ts
+++ src/routes/MainPage.ts
@@ -3,7 +3,7 @@ import { section, h1, p, a, b, header } from "markup-as-js"
 const Main = () => section(
   header(
     h1("Tyler Mercer"),
-    p("Humanistic Software Engineer"),
+    p("Humanistic Software Developer"),
     p(a({href: "/projects"}, b("View my projects")))
   )
 );
