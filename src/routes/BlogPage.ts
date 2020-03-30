0d25f48f82339abe731039bf2fb5d2ae136ea821
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-30 13:28:19 -0600

    Flesh out projects page

--- src/routes/BlogPage.ts
+++ src/routes/BlogPage.ts
@@ -1,5 +1,10 @@
-import { h1, section, header } from 'markup-as-js';
+import { h1, section, header, p } from 'markup-as-js';
 
-const BlogPage = () => section(header(h1("Blog")));
+const BlogPage = () => section(
+  header(
+    h1("Blog"), 
+    p("WIP")
+  ),
+);
 
 export default BlogPage;
