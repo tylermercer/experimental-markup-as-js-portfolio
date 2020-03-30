f9b8be688d041ddd26254bc6b37c28df8e2d86aa
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-30 13:17:27 -0600

    Fix bug where page would load projects list before projects link was clicked

--- src/routes/ProjectsPage.ts
+++ src/routes/ProjectsPage.ts
@@ -2,16 +2,16 @@ import { h1, header, ul, section, li, a } from 'markup-as-js';
 
 const QUERY = 'https://api.github.com/users/tylermercer/repos?sort=pushed&direction=desc';
 
-const projects = fetch(QUERY)
+const getProjects = () => fetch(QUERY)
   .then(res => res.json())
   .then(repos => ul(
-    ...repos.map((r:any) => li(a({href: "url"}, r.name)))
+    ...repos.map((r:any) => li(a({href: r.html_url, target: "_blank"}, r.name)))
   ));
 
 const ProjectsPage = () => section(
   header(
     h1("Projects"),
-    projects
+    getProjects()
   ),
 );
 
