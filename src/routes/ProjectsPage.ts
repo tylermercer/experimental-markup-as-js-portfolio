a7674c4b1edfca89daf3ed293e30f2409c17bf46
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-30 13:28:19 -0600

    Flesh out projects page

--- src/routes/ProjectsPage.ts
+++ src/routes/ProjectsPage.ts
@@ -1,11 +1,15 @@
-import { h1, header, ul, section, li, a } from 'markup-as-js';
+import { h1, header, h3, section, a, aside, p, small } from 'markup-as-js';
 
 const QUERY = 'https://api.github.com/users/tylermercer/repos?sort=pushed&direction=desc';
 
 const getProjects = () => fetch(QUERY)
   .then(res => res.json())
-  .then(repos => ul(
-    ...repos.map((r:any) => li(a({href: r.html_url, target: "_blank"}, r.name)))
+  .then(repos => section(
+    ...repos.filter((r:any) => r.description).map((r:any) => aside(
+      h3(a({href: r.html_url, target: "_blank"}, r.name)),
+      p((r.description || "")),
+      p(small(r.language))
+    ))
   ));
 
 const ProjectsPage = () => section(
