c715d0b99b0bf6ec12a4a5d1ba7456efb07d4cbe
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-31 23:04:18 -0600

    Start building SSR

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,44 @@ import page from 'page';
 import App from './App';
 import routes from './routes';
 
+const getAppContainer = () => {
+  let appContainer = document.getElementById("app") as Element;
+  if (!appContainer) console.error("No #route element found in hydrated app");
+  return appContainer;
+}
+
 const currentPath = new SimpleObservable("/");
 
-const app = App(currentPath);
+let initialRouteNode: Element | null = document.getElementById("route");
 
-document.getElementById("app")?.appendChild(app);
+let routeNode: Element;
 
-let routeNode: Element = document.getElementById("route") as Element;
+if (initialRouteNode) {
+  console.log("App was SSRed, hydrating");
+  //App was SSRed, so we need to hydrate it
 
-if (!routeNode) console.error("No #route element found");
+  //Copy SSRed route
+  const liveApp = App(currentPath);
+  let deadRouteNode = initialRouteNode;
+  routeNode = liveApp.querySelector("#route") as Element;
+  if (!routeNode) console.error("No #route element found in hydrated app");
+  routeNode.parentElement?.replaceChild(deadRouteNode, routeNode);
+
+  //Insert live app
+  let appContainer = getAppContainer();
+  appContainer.innerHTML = ""
+  appContainer.appendChild(liveApp);
+}
+else {
+  console.log("App was not SSRed, building");
+  //App was not SSRed, so we need to build it from scratch
+  const app = App(currentPath);
+  getAppContainer().appendChild(app);
+  routeNode = document.getElementById("route") as Element;
+  if (!routeNode) console.error("No #route element found in app");
+}
+
+let isFirstLoad = true;
 
 if (routeNode) {
   for (let route of routes) {
